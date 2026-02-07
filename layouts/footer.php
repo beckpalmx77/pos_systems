c322fdbdb01e43080237fdd99d6401c6e30196de
beckpalmx77 <beckpalmx@gmail.com>
2026-02-07 17:03:27 +0700

    Update footer.php

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,6 @@
-</div> <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
 
